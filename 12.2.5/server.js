3d4a16ce2938e3730910452acba9e11005f9ca4f
tom2u <tbrodhead@2u.com>
2020-05-06 13:39:20 -0500

    12.2.5

---
+++ 12.2.5/server.js
@@ -0,0 +1,89 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const urlencodedParser = express.urlencoded({ extended: false });
+const jsonParser = express.json();
+
+// Express middleware
+
+app.use(urlencodedParser);
+app.use(jsonParser);
+
+// Connect to database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log("Connected to the election database.");
+});
+
+// // Get all candidates
+// app.get("/api/candidates", (req, res) => {
+//   const sql = `SELECT * FROM candidates`;
+//   const params = [];
+//   db.all(sql, params, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+
+//     res.json({
+//       message: "success",
+//       data: rows,
+//     });
+//   });
+// });
+
+// Get single candidate
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// // Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Each link is to the state of the webpage at the END of the indicated lesson.
 [12.1.6](https://github.com/tom2u/coding-online-module-12/tree/master/12.1.6)  
 [12.2.3](https://github.com/tom2u/coding-online-module-12/tree/master/12.2.3)  
 [12.2.4](https://github.com/tom2u/coding-online-module-12/tree/master/12.2.4)  
+[12.2.5](https://github.com/tom2u/coding-online-module-12/tree/master/12.2.5)  
